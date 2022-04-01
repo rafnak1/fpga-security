79544d73ccf2c7448429d035a9467461a25814cb
rafael nakata <rafaelk.nakata@gmail.com>
2022-04-01 11:38:01 -0300

    create the mosquitto test

---
+++ server.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+import time
+
+user = "grupo2-bancadaA2"
+passwd = "L@Bdygy2A2"
+
+Broker = "labdigi.wiseful.com.br"
+Port = 80
+KeepAlive = 60
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Conectado com codigo " + str(rc))
+    client.subscribe(user+"/asdf", qos=0)
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+client = mqtt.Client(protocol=mqtt.MQTTv31)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set(user, passwd)
+
+client.connect(Broker, Port, KeepAlive)
+client.loop_start()
+
+# Primeiro publish é geralmente ignorado
+client.publish(user + "/asdf", payload="1", qos=0, retain=False)
+
+print("Servidor iniciou")
+while (True):
+    pass
+
+client.loop_stop()
+client.disconnect()
+
+print("done")
