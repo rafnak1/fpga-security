6d4080dc05159e604fb5bc23f6df203b0e5b85da
rafael nakata <rafaelk.nakata@gmail.com>
2022-04-01 21:21:49 -0300

    Rascunho do servidor

--- mosquitto.py
+++ mosquitto.py
@@ -5,14 +5,15 @@ import time
 user = ""
 passwd = ""
 
-Broker = "labdigi.wiseful.com.br"
-Port = 80
+Broker = "test.mosquitto.org"
+Port = 1883
 KeepAlive = 60
 
 
 def on_connect(client, userdata, flags, rc):
     print("Conectado com codigo " + str(rc))
-    client.subscribe(user+"/asdf", qos=0)
+    client.subscribe("parafernalia1", qos=0)
+    client.subscribe("parafernalia2", qos=0)
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
--- server.py
+++ server.py
@@ -8,13 +8,52 @@ Broker = "labdigi.wiseful.com.br"
 Port = 80
 KeepAlive = 60
 
+# tópicos temporários!
+var2topic = {
+    # 7 entradas
+    'com0'   : 'S0',
+    'com1'   : 'S1',
+    'com2'   : 'S2',
+    'com3'   : 'S3',
+    'trig'   : 'S4',
+    'alarme' : 'S5',
+    'abriu'  : 'S6',
+    # 8 saídas
+    'BDnormal0' : 'E0'
+}
+
 
 def on_connect(client, userdata, flags, rc):
+    global in_var2topic
     print("Conectado com codigo " + str(rc))
     client.subscribe(user+"/asdf", qos=0)
+    for topic in in_var2topic.values:
+        client.subscribe(user+"/"+topic, qos=0)
 
 def on_message(client, userdata, msg):
+    global in_var2topic, user
     print(msg.topic+" "+str(msg.payload))
+
+    com = [False for i in range(4)]
+    if msg.topic = user+"/S0":
+        com[0] = msg.payload == b'1'
+    if msg.topic = user+"/S1":
+        com[1] = msg.payload == b'1'
+    if msg.topic = user+"/S2":
+        com[2] = msg.payload == b'1'
+    if msg.topic = user+"/S3":
+        com[3] = msg.payload == b'1'
+
+    # Sinais internos
+    ID = 0
+    BDn = [[False for j in range(4)] for i in range(16)]
+    BDp = [[False for j in range(4)] for i in range(16)]
+    sizeN = [0 for _ in range(16)]
+    sizeP = [0 for _ in range(16)]
+
+    if msg.topic == user+"/trig" and msg.payload == '1':
+        ID = sum([com[i]*2**i for i in range(3, -1, -1)])
+        publish4()
 
 
 client = mqtt.Client(protocol=mqtt.MQTTv31)
@@ -33,7 +72,3 @@ print("Servidor iniciou")
 while (True):
     pass
 
-client.loop_stop()
-client.disconnect()
-
-print("done")
